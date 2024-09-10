7ef36f0defc710742096e95f68b173de5f7df3f4
SamaraLisboa <samara.bueno@sptech.school>
2024-09-10 15:28:25 -0300

    animação dos itens de lista do cadastro e login, adição de caracteres válidos

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -5,5 +5,8 @@ create table empresa (
 id int primary key auto_increment,
 cnpj char(14),
 email varchar(100),
-senha varchar(30)
-); 
+senha varchar(30),
+
+constraint uk_email unique (email),
+constraint uk_cpj unique (cnpj)
+);
