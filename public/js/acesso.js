6a66d1a52be5b91d5ed0abe6eebd7082c59f3b6d
Rafaelsmlee <rafael.lee@sptech.com>
2024-10-30 15:45:45 -0300

    redirecionamento dashboard

--- public/js/acesso.js
+++ public/js/acesso.js
@@ -93,7 +93,6 @@ function cadastrar() {
       if (resposta.ok) {
         cardErro.style.display = "block";
         mensagemErro.innerHTML = "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
-        window.location = "dashboard\dashboard.html"
         painelDeslizante.style.transform = 'translateX(100%)';
       } else {
         cardErro.style.display = "block";
@@ -113,3 +112,48 @@ function cadastrar() {
     cardErro.style.display = "none"
   }
 }
+
+fetch("/usuarios/autenticar", {
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+      emailServer: emailVar,
+      senhaServer: senhaVar
+  })
+}).then(function (resposta) {
+  console.log("ESTOU NO THEN DO entrar()!")
+
+  if (resposta.ok) {
+      console.log(resposta);
+
+      resposta.json().then(json => {
+          console.log(json);
+          console.log(JSON.stringify(json));
+          sessionStorage.EMAIL_USUARIO = json.email;
+          sessionStorage.NOME_USUARIO = json.nome;
+          sessionStorage.ID_USUARIO = json.id;
+          sessionStorage.AQUARIOS = JSON.stringify(json.aquarios)
+
+          setTimeout(function () {
+              window.location = "../dashboard/dashboard.html";
+          }, 1000); // apenas para exibir o loading
+
+      });
+
+  } else {
+
+      console.log("Houve um erro ao tentar realizar o login!");
+
+      resposta.text().then(texto => {
+          console.error(texto);
+          finalizarAguardar(texto);
+      });
+  }
+
+}).catch(function (erro) {
+  console.log(erro);
+})
+
+return false;
