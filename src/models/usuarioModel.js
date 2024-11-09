ed224bc05785cfc0d00292c151ab17735d780db9
SamaraLisboa <samara.bueno@sptech.school>
2024-11-09 12:32:39 -0300

    ajustes no direcionamento acesso

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,15 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT id, cnpj, email FROM empresa WHERE email = '${email}' AND senha = '${senha}';
+              SELECT idEmpresa AS id, nomeFantasia AS nome, email, 'Empresa' AS tipoUsuario
+        FROM Empresa
+        WHERE email = '${email}' AND senha = '${senha}'
+        
+        UNION
+        
+        SELECT idFuncionario AS id, nome, email, 'Funcionario' AS tipoUsuario
+        FROM Usuario
+        WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
