d22585cf57514c8ebea6126edbd16db78fde10ba
MuriloNasciment0 <murilo.barros@sptech.school>
2024-12-11 17:27:53 -0300

    acessando com senha padrão

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -5,16 +5,45 @@ function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha);
 
     var instrucaoSql = `
-    Select Pessoa.idPessoa, Pessoa.nome, Usuario.email, Usuario.tipoUsuario
-        from Pessoa 
-        inner join Usuario  on fkpessoa = idPessoa
-        WHERE email = '${email}' AND senha = '${senha}' 
-        
-        union all 
-        
-		SELECT idEmpresa AS id, nomeFantasia AS nome, email, tipoUsuario
-        FROM Empresa
-        WHERE email = '${email}' AND senha = '${senha}';
+    SELECT 
+    Pessoa.idPessoa, 
+    Pessoa.nome, 
+    Usuario.email, 
+    Usuario.tipoUsuario
+FROM 
+    Pessoa 
+INNER JOIN 
+    Usuario  
+    ON Usuario.fkPessoa = Pessoa.idPessoa
+WHERE 
+    Usuario.email = '${email}' 
+    AND 
+    (
+        -- Verificação se a senha é nula ou não
+        (
+            Usuario.senha IS NOT NULL 
+            AND Usuario.senha = '${senha}'
+        ) 
+        OR 
+        (
+            Usuario.senha IS NULL 
+            AND Usuario.senhaPadrao = '${senha}'
+        )
+    )
+
+UNION ALL 
+
+SELECT 
+    idEmpresa AS id, 
+    nomeFantasia AS nome, 
+    email, 
+    tipoUsuario
+FROM 
+    Empresa
+WHERE 
+    Empresa.email = '${email}' 
+    AND 
+    Empresa.senha = '${senha}'
         
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
